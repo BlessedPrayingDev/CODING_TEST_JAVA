c1dec5523bc436c11990c801b657b166492de1bb
BlessedPrayingDevGru <gasaret@gmail.com>
2023-07-05 12:01:31 +0900

    Coding Test by using JAVA

---
+++ WEEK1/DAY1.java
@@ -0,0 +1,10 @@
+package WEEK1;
+
+public class DAY1 {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = a + b;
+        System.out.println(c);
+    }
+}
