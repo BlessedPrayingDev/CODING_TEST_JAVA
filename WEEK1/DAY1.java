40b289be91a8bfb6cc55ce2999c9a4242261c2a8
BlessedPrayingDevGru <gasaret@gmail.com>
2023-07-10 17:27:47 +0900

    Coding Test by using JAVA

--- WEEK1/DAY1.java
+++ WEEK1/DAY1.java
@@ -2,6 +2,6 @@ package WEEK1;
 
 public class DAY1 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!"); // Hello World 출력해보기~!!
     }
 }
