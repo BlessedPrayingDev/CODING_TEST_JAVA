e0b6afa5dc8b146dd0c3a7cb54fbad82b8426994
BlessedPrayingDevGru <gasaret@gmail.com>
2023-08-03 09:05:28 +0900

    코테준비(자바) - 2주차/DAY3

---
+++ WEEK2/DAY3.java
@@ -0,0 +1,11 @@
+package WEEK2;
+
+import java.util.Scanner;
+
+public class DAY3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int year = scanner.nextInt();
+        System.out.println(year - 543);
+    }
+}
