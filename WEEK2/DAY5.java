5b82eb7812507ca872d542d6b1c171926a0e1443
BlessedPrayingDevGru <gasaret@gmail.com>
2023-08-07 09:54:14 +0900

    코테준비(자바) - 2주차/DAY5

---
+++ WEEK2/DAY5.java
@@ -0,0 +1,15 @@
+package WEEK2;
+
+import java.util.Scanner;
+
+public class DAY5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(a * (b % 10));
+        System.out.println(a * ((b / 10) % 10));
+        System.out.println(a * (b / 100));
+        System.out.println(a * b);
+    }
+}
