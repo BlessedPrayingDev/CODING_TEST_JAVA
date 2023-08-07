32ff5cabb34a8ad3d2bb0b5cb9e2ef881e9d501e
BlessedPrayingDevGru <gasaret@gmail.com>
2023-08-07 10:02:15 +0900

    코테준비(자바) - 3주차/DAY1

---
+++ WEEK3/DAY1.java
@@ -0,0 +1,13 @@
+package WEEK3;
+
+import java.util.Scanner;
+
+public class DAY1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long a = scanner.nextLong();
+        long b = scanner.nextLong();
+        long c = scanner.nextLong();
+        System.out.println(a + b + c);
+    }
+}
